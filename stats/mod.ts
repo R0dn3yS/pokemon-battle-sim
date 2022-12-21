93ed17545ced7f920d8ffd7ad3755ab86cc160d6
Rodney van den Velden <rodneymichael2002@gmail.com>
2022-12-21 01:29:09 +0100

    Stat calculation without natures

--- stats/mod.ts
+++ stats/mod.ts
@@ -1,14 +1,18 @@
 export class Stats {
   hp: number;
-  // atk: number;
-  // def: number;
-  // spatk: number;
-  // spdef: number;
-  // spd: number;
+  atk: number;
+  def: number;
+  spatk: number;
+  spdef: number;
+  spd: number;
 
   constructor(base: BaseStats, iv: IV, ev: EV, level: number) {
     this.hp = (Math.floor(((2 * base.hp + iv.hp + (Math.floor(ev.hp / 4))) * level) / 100)) + (level + 10);
-    // this.atk = ()
+    this.atk = (Math.floor((((2 * base.atk) + iv.atk + (Math.floor(ev.atk / 4))) * level) / 100)) + 5;
+    this.def = (Math.floor((((2 * base.def) + iv.def + (Math.floor(ev.def / 4))) * level) / 100)) + 5;
+    this.spatk = (Math.floor((((2 * base.spatk) + iv.spatk + (Math.floor(ev.spatk / 4))) * level) / 100)) + 5;
+    this.spdef = (Math.floor((((2 * base.spdef) + iv.spdef + (Math.floor(ev.spdef / 4))) * level) / 100)) + 5;
+    this.spd = (Math.floor((((2 * base.spd) + iv.spd + (Math.floor(ev.spd / 4))) * level) / 100)) + 5;
   }
 }
 
